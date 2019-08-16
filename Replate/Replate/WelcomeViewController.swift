a5cc847f1abf1b58ef10caee6b4d488b8f5dd0d8
Julian A. Fordyce <julian.a.fordyce@gmail.com>
2019-08-16 13:41:45 -0400

    rename default view controller to "WelcomeViewController"

--- Replate/Replate/WelcomeViewController.swift
+++ Replate/Replate/WelcomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WelcomeViewController.swift
 //  Replate
 //
 //  Created by Julian A. Fordyce on 8/16/19.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WelcomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
