f93f2ca7948923a8f8c43e9d9531de9aad8f7822
Julian A. Fordyce <julian.a.fordyce@gmail.com>
2019-08-16 12:56:53 -0400

    create a Bearer model file to represent the token from the API

--- Replate/Replate/APIController.swift
+++ Replate/Replate/APIController.swift
@@ -7,3 +7,25 @@
 //
 
 import Foundation
+import UIKit
+
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+}
+
+enum NetworkError: Error {
+    case noAuth
+    case badAuth
+    case otherError
+    case badData
+    case noDecode
+}
+
+
+class APIController {
+    
+    private let baseURL = URL(string: "https://bw-replate.herokuapp.com/api/auth/business/register")!
+    var bearer: Bearer?
+}
---
+++ Replate/Replate/Bearer.swift
@@ -0,0 +1,9 @@
+//
+//  Bearer.swift
+//  Replate
+//
+//  Created by Julian A. Fordyce on 8/16/19.
+//  Copyright © 2019 Glas Labs. All rights reserved.
+//
+
+import Foundation
