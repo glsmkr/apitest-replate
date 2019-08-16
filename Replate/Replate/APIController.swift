0af71f4fe1195923d1a3c1c04988858a4c013bcc
Julian A. Fordyce <julian.a.fordyce@gmail.com>
2019-08-16 13:30:29 -0400

    complete basic register/login functionality

--- Replate/Replate/APIController.swift
+++ Replate/Replate/APIController.swift
@@ -26,12 +26,91 @@ enum NetworkError: Error {
 
 class APIController {
     
-    private let baseURL = URL(string: "https://bw-replate.herokuapp.com/api/auth/business/register")!
+    private let baseURL = URL(string: "https://bw-replate.herokuapp.com/api")!
     var bearer: Bearer?
     
     func register(with user: User, completion: @escaping (Error?) -> ()) {
+        let registerURL = baseURL.appendingPathComponent("/auth/business/register")
         
+        var request = URLRequest(url: registerURL)
+        request.httpMethod = HTTPMethod.post.rawValue
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let jsonEncoder = JSONEncoder()
+        
+        do {
+            let data = try jsonEncoder.encode(user)
+            request.httpBody = data
+        } catch {
+            print("Error encoding the user: \(error)")
+            completion(error)
+            return
+        }
+        
+        URLSession.shared.dataTask(with: request) { (_, response, error) in
+            if let response = response as? HTTPURLResponse,
+                response.statusCode != 200 {
+                completion(NSError(domain: "", code: response.statusCode, userInfo: nil))
+                return
+            }
+            
+            if let error = error {
+                completion(error)
+                return
+            }
+            
+            completion(nil)
+        }.resume()
     }
+    
+    
+    func signIn(with user: User, completion: @escaping (Error?) -> ()) {
+        let loginURL = baseURL.appendingPathComponent("/auth/business/login")
+        
+        var request = URLRequest(url: loginURL)
+        request.httpMethod = HTTPMethod.post.rawValue
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let jsonEncoder = JSONEncoder()
+        do {
+            let data = try jsonEncoder.encode(user)
+            request.httpBody = data
+        } catch {
+            print("Error encoding user: \(error)")
+            completion(error)
+            return
+        }
+        
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let response = response as? HTTPURLResponse,
+                response.statusCode != 200 {
+                completion(NSError(domain: "", code: response.statusCode, userInfo: nil))
+                return
+            }
+            
+            if let error = error {
+                completion(error)
+                return
+            }
+            
+            guard let data = data else {
+                completion(NSError())
+                return
+            }
+            
+            let decoder = JSONDecoder()
+            do {
+                self.bearer = try decoder.decode(Bearer.self, from: data)
+            } catch {
+                print("Error decoding bearer token: \(error)")
+                completion(error)
+                return
+            }
+            
+            completion(nil)
+        }.resume()
+    }
+    
     
     
 }
