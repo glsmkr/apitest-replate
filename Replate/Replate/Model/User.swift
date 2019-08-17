0c6804e6a054b44dfe702563aa08fa31107442d1
Julian A. Fordyce <julian.a.fordyce@gmail.com>
2019-08-16 20:07:02 -0400

    set up welcome view controller

--- Replate/Replate/Model/User.swift
+++ Replate/Replate/Model/User.swift
@@ -16,4 +16,13 @@ struct User: Codable {
     let email: String
     let phone: Int
     
+    init(username: String, password: String, organizationName: String = "Test", address: String = "123 Candy Lane", email: String = "test@gmail.com", phone: Int = 1234567890) {
+        self.username = username
+        self.password = password
+        self.address = address
+        self.organizationName = organizationName
+        self.email = email
+        self.phone = phone
+    }
+    
 }
--- Replate/Replate/WelcomeViewController.swift
+++ Replate/Replate/WelcomeViewController.swift
@@ -8,6 +8,11 @@
 
 import UIKit
 
+enum LoginType {
+    case login
+    case register
+}
+
 class WelcomeViewController: UIViewController {
 
     override func viewDidLoad() {
@@ -16,6 +21,31 @@ class WelcomeViewController: UIViewController {
     }
     
     @IBAction func registerOrLogin(_ sender: Any) {
+        guard let username = usernameTextField.text,
+            let password = passwordTextField.text,
+            !username.isEmpty,
+            !password.isEmpty else { return }
+        
+        let user = User(username: username, password: password)
+        
+        if signinType == .register {
+            apiController.register(with: user) { error in
+                if let error = error {
+                    print("Error occurred during sign up: \(error)")
+          } // else {
+//                    DispatchQueue.main.async {
+//                        let alertController = UIAlertController(title: "Sign Up Successful", message: "Now please log in.", preferredStyle: .alert)
+//                        let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+//                        alertController.addAction(alertAction)
+//                        self.present(alertController, animated: true, completion: {
+//                            self.signinType = .login
+//                            self.loginSegmentedControl.selectedSegmentIndex = 1
+//                            self.signInButton.setTitle("Sign In", for: .normal)
+//                        })
+//                    }
+//                }
+            }
+        }
     }
     
     @IBAction func changeSignType(_ sender: Any) {
@@ -30,6 +60,9 @@ class WelcomeViewController: UIViewController {
     @IBOutlet weak var signInButton: UIButton!
     
     @IBOutlet weak var loginSegmentedControl: UISegmentedControl!
+    
+    var apiController = APIController()
+    var signinType = LoginType.register
     
 }
 
